2776a6a4f39d1081d7c87a50f5bf03de8d15b25e
krnrmnk <karinrom.2000@gmail.com>
2024-08-20 21:57:13 +0300

    Update script.js
    Доработка дз-4

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+/* Если кликнуть на элемент списка, он зачеркивается */
+/* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
+/* Очищать input после добавления нового элемента в список */
+/* Пустые элементы не должны добавляться */
+
 const inputElement = document.querySelector('#input');
 const itemsElement = document.querySelector('#items');
 
-inputElement.addEventListener('keyup', function(event) {
+inputElement.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
+
         const newItem = inputElement.value;
-    }
-    /* Если кликнуть на элемент списка, он зачеркивается */
-    /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
-
-    itemElement.addEventListener('click', function() {
-        const clickedItem = event;
-        if (clickedItem.classList.contains('done')) {
-            clickedItem.classList.remove('done');
-        } else {
-            clickedItem.classList.add('done');
-        }
-    });
-
-    /* Очищать input после добавления нового элемента в список */
-    if (newItem !== '') {
         const itemElement = document.createElement('div');
-        itemElement.textContent = newItem;
-    }
+        itemElement.classList.add('item');
 
-    /* Пустые элементы не должны добавляться */
-    itemsElement.append(itemElement);
-    inputElement.value = '';
+        itemElement.addEventListener('click', function() {
+            itemElement.classList.toggle('done');
+        })
+
+        itemElement.textContent = newItem;
+        if (newItem !== '') {
+            itemsElement.append(itemElement);
+        }
+
+        inputElement.value = '';
+    }
 })
